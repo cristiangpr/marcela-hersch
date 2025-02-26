4f2d2aafbfa5787e2e41f075e271c0fa9fa96e36
cristiangpr <cristiangp777@gmail.com>
2025-02-26 12:22:30 -0600

    tesis

--- src/app/tesis/tesis/page.tsx
+++ src/app/tesis/tesis/page.tsx
@@ -1,5 +1,5 @@
 import GalleryTrigger from '@/components/Trigger'
-import { Container, Stack, Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import { Box } from '@mui/material'
 
 export default function Tesis() {
